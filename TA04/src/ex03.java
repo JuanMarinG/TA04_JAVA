d2e2169e3ad0565acb785dff3dd4e90d269c9bdb
JuanMarinG <108723755+JuanMarinG@users.noreply.github.com>
2022-07-07 11:03:47 +0200

    Commit ejercicio 3

---
+++ TA04/src/ex03.java
@@ -0,0 +1,21 @@
+
+public class ex03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int X=2;
+		int Y=4;
+		double N=6;
+		double M=8;
+		double resultado=0;
+		
+		System.out.println("El valor de X es "+X);
+		System.out.println("El valor de X es "+Y);
+		System.out.println("El valor de X es "+N);
+		System.out.println("El valor de X es "+M);
+		
+		resultado=X+Y;
+		System.out.println("El resultado de la suma es "+resultado;
+	}
+
+}
