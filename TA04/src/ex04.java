3d01e6bb2c32edb4bc205f4130eff19ee7d9d1d7
JuanMarinG <108723755+JuanMarinG@users.noreply.github.com>
2022-07-07 11:43:30 +0200

    Commit Ejercicio 04

--- TA04/src/ex04.java
+++ TA04/src/ex04.java
@@ -3,7 +3,15 @@ public class ex04 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int N=10;
+		int resultado=0;
+		
+		resultado=N+77;
+		System.out.println("Al incrementar en 77 el resultado es "+resultado);
+		resultado=N-3;
+		System.out.println("Al decrementar en -3 el resultado es "+resultado);
+		resultado=N*2;
+		System.out.println("Al duplicar su valor es "+resultado);
 	}
 
 }
