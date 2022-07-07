8ca22b4320e7d73f8811cf93fea5921c07d8420d
JuanMarinG <108723755+JuanMarinG@users.noreply.github.com>
2022-07-07 11:32:11 +0200

    Commit Ejercicio 3

--- TA04/src/ex03.java
+++ TA04/src/ex03.java
@@ -8,14 +8,50 @@ public class ex03 {
 		double N=6;
 		double M=8;
 		double resultado=0;
+		double suma=X+Y+N+M;
+		
 		
 		System.out.println("El valor de X es "+X);
-		System.out.println("El valor de X es "+Y);
-		System.out.println("El valor de X es "+N);
-		System.out.println("El valor de X es "+M);
+		System.out.println("El valor de Y es "+Y);
+		System.out.println("El valor de N es "+N);
+		System.out.println("El valor de M es "+M);
 		
 		resultado=X+Y;
-		System.out.println("El resultado de la suma es "+resultado;
+		System.out.println("El resultado de la suma es "+resultado);
+		resultado=X-Y;
+		System.out.println("El resultado de la diferencia es "+resultado);
+		resultado=X*Y;
+		System.out.println("El resutlado de la multiplicacion es "+resultado);
+		resultado=X/Y;
+		System.out.println("El resutlado de la division es "+resultado);
+		resultado=X%Y;
+		System.out.println("El resutlado del resto es "+resultado);
+		resultado=N+M;
+		System.out.println("El resutlado de la suma es "+resultado);
+		resultado=N-M;
+		System.out.println("El resutlado de la resta es "+resultado);
+		resultado=N*M;
+		System.out.println("El resutlado de la multiplicacion es "+resultado);
+		resultado=N/M;
+		System.out.println("El resutlado de la division es "+resultado);
+		resultado=N%M;
+		System.out.println("El resutlado del resto es "+resultado);
+		resultado=X+N;
+		System.out.println("El resutlado de la suma es "+resultado);
+		resultado=Y/M;
+		System.out.println("El resutlado de la division es "+resultado);
+		resultado=Y%M;
+		System.out.println("El resutlado del resto es "+resultado);
+		
+		System.out.println("El resultado de la suma de todas las variables es "+suma);
+		
+		System.out.println("El doble de X es " + 2*X);
+		System.out.println("El doble de Y es " + 2*Y);
+		System.out.println("El doble de N es " + 2*N);
+		System.out.println("El doble de M es " + 2*M);
+		
+		System.out.println(X + " * " + Y + " * " + N + " * " + M + " = " + (X*Y*M*N)); 
+      
 	}
 
 }
