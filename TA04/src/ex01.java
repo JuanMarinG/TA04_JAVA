30ae0eb492b8ab5ad7c268bc5863818ec3842bac
JuanMarinG <108723755+JuanMarinG@users.noreply.github.com>
2022-07-07 10:35:16 +0200

    Ejercicio 1 UD04

--- TA04/src/ex01.java
+++ TA04/src/ex01.java
@@ -3,7 +3,19 @@ public class ex01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("HolaMundo");
+		int valor1=10,	valor2=5,	resultado=0;
+		
+		
+		resultado= valor1+valor2;
+		System.out.println("El resultado de la suma es " +resultado);
+		resultado= valor1-valor2;
+		System.out.println("El resultado de la resta es " +resultado);
+		resultado= valor1*valor2;
+		System.out.println("El resultado de la multiplicacion es " +resultado);
+		resultado= valor1/valor2;
+		System.out.println("El resultado de la division es " +resultado);
+		resultado= valor1%valor2;
+		System.out.println("El resultado del resto de la division es " +resultado);
 	}
 
 }
