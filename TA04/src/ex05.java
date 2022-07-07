853c34003432ec3239d1bb5fc5d2975b40398298
JuanMarinG <108723755+JuanMarinG@users.noreply.github.com>
2022-07-07 11:58:47 +0200

    Commit Ejercicio 05

---
+++ TA04/src/ex05.java
@@ -0,0 +1,17 @@
+
+public class ex05 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int A=1;
+		int B=2;
+		int C=3;
+		int D=4;
+		
+		System.out.println ("El valor de B es " + C);
+		System.out.println("El valor de C es " + A);
+		System.out.println ("El valor de A es " + D);
+		System.out.println("El valor de D es " + B);
+	}
+
+}
