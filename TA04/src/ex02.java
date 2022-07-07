667ea46d0c7974798ce843d33e0546846f4a2028
JuanMarinG <108723755+JuanMarinG@users.noreply.github.com>
2022-07-07 10:55:22 +0200

    Commit ejercicio 2

---
+++ TA04/src/ex02.java
@@ -0,0 +1,24 @@
+
+public class ex02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int n=1;
+		double a=2;
+		char c='z';
+		double resultado=0;
+		
+		System.out.println("El valor de n es "+n);
+		System.out.println("El valor de a es "+a);
+		System.out.println("El valor de c es "+c);
+		
+		resultado= n+a;
+		System.out.println("El resultado de la suma es "+resultado);
+		resultado= n-a;
+		System.out.println("El resultado de la suma es "+resultado);
+		resultado= c='!';
+		System.out.println("El resultado numerico de char es "+resultado);
+	}
+
+}
